50078c2d4d1d6c407869f83f390b91cd51908946
serr <sc7227484@gmail.com>
2025-08-13 23:48:12 +0300

    добавил бинды для работы с терминалом, вкладками

--- init.lua
+++ init.lua
@@ -28,6 +28,15 @@ vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 -- alt+z - vim.opt.wrap = false/true
 vim.keymap.set('n', '<A-z>', function() vim.opt.wrap = not vim.opt.wrap:get() end, { silent = true })
+-- alt+` - открыть терминал в новой вкладке + сразу заходит в режим терминала
+vim.keymap.set('n', '<A-`>', ':tabnew | terminal<CR>i', { noremap = true, silent = true })
+-- Выход из режима терминала используя esc
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
+-- alt+3 - Перейти на пред. вкладку
+vim.keymap.set('n', '<A-3>', ':tabprevious<CR>', { noremap = true, silent = true })
+-- alt+4 - Перейти на след. вкладку
+vim.keymap.set('n', '<A-4>', ':tabnext<CR>', { noremap = true, silent = true })
+
 
 -- Настройки плагинов
 
