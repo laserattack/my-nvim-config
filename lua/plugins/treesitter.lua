c6aec0a5a456e9e633e21b0c593d7f9b91b16648
serr <sc7227484@gmail.com>
2025-08-14 18:54:26 +0300

    some changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "zig" },
+            ensure_installed = { "c", "cpp", "lua", "zig", "python" },
             sync_install = false,
             auto_install = false,
             highlight = {
