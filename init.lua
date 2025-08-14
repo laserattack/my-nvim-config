0da955b58b3e2b591b6b89ccc77f7679657721b1
serr <sc7227484@gmail.com>
2025-08-14 18:45:22 +0300

    some changes

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@
 ---- https://github.com/nvim-tree/nvim-tree.lua - плагин для отображения дерева директорий
 ---- https://github.com/nvim-tree/nvim-web-devicons - плагин для красивых иконок в дереве директорий
 ---- https://github.com/rebelot/kanagawa.nvim - плагин для установки цветовой темы
-
+---- https://github.com/nvim-treesitter/nvim-treesitter - плагин для умной подстветки ЯП
 
 -- Комбинации клавиш
 --- Базовые
