b013d28ed15899f7435d1bec4d9fd6ffc9ca9f12
serr <sc7227484@gmail.com>
2025-08-15 17:12:48 +0300

    настроил автодополнение

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,25 +18,9 @@ return {
             },
             -- Маппинги
             mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                -- Вызов автодополнения
-                ['<C-Space>'] = cmp.mapping.complete(),
-                -- Отмена автодополнения
-                ['<C-e>'] = cmp.mapping.abort(),
-                -- Подтверждение выбора - enter
+                -- enter чтобы выбрать нужный вариант
+                -- переключение вариантов на стрелочки
                 ['<CR>'] = cmp.mapping.confirm({select = true}),
-                -- Переключение между вариантами автодополнения (tab - вперед, shift+tab - назад)
-                ["<Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then cmp.select_next_item() else fallback() end
-                    end, {"i", "s"}
-                ),
-                ["<S-Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then cmp.select_prev_item() else fallback() end
-                    end, {"i", "s"}
-                )
             }),
             -- Источник автодополнения - текущий буффер
             sources = cmp.config.sources({{name = "nvim_lsp"}}, {{name = 'buffer'}})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,7 @@
 
 -- Настройки LSP-серверов
 local ls_settings = {
+    -- lsp сервер для Lua
     lua_ls = {
         cmd = { vim.fn.expand('~/.config/nvim/lsp/lua/bin/lua-language-server') },
         settings = {
@@ -11,7 +12,7 @@ local ls_settings = {
                 telemetry = { enable = false },
             },
         }
-    }
+    },
 }
 
 -- Общие настройки
@@ -28,12 +29,33 @@ local general_settings = function(_, _)
     })
 end
 
+-- Функция для вывода уведомлений
+local function notify(msg)
+    print(msg)
+    vim.notify("")
+end
+
+-- Функция для проверки наличия серверов
+local function check_lsp_path(server_name, binary_path)
+    if vim.fn.executable(binary_path) ~= 1 then
+        notify(string.format("Language server '%s' not found by path: %s", server_name, binary_path))
+        return false
+    end
+    return true
+end
+
 return {
     'neovim/nvim-lspconfig',
     config = function()
         for server_name, server_settings in pairs(ls_settings) do
+            if not check_lsp_path(server_name, server_settings.cmd[1]) then
+               goto continue
+            end
+
             server_settings.on_attach = general_settings
             require("lspconfig")[server_name].setup(server_settings)
+
+            ::continue::
         end
     end
 }
